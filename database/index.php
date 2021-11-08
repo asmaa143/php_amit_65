ffb11b475e459a3d294f8185e5324902edf9fdd3
AsmaaMohamedKhraiba <Asm2kh@gmail.com>
2021-11-08 22:02:51 +0200

    add country loader

--- database/index.php
+++ database/index.php
@@ -2,10 +2,10 @@
 require_once("conn.php");
 session_start();
 
-$query="SELECT * FROM country LIMIT 0,20;";
-$stm=$pdo->query($query);
+$query = "SELECT * FROM country LIMIT 0,20;";
+$stm = $pdo->query($query);
 
- $allRecords=$stm->fetchAll(PDO::FETCH_ASSOC);
+$allRecords = $stm->fetchAll(PDO::FETCH_ASSOC);
 
 // foreach($allRecords as $row){
 //     print_r($row);
@@ -24,56 +24,106 @@ $stm=$pdo->query($query);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/all.min.css">
     <title>index</title>
 </head>
+
 <body>
-<div class="container my-5">
-    <div class="row">
-        <div class="col-12">
-                   <div class="card shadow border">
-                        <div class="card-body">
-                            <h2 class="text-center">Countries</h2>
-                            <hr>
-                             <?php include_once("success.php")?>
-                            <table class="table table-bordered table-striped">
-                                  <thead>
-                                      <tr>
-                                          <th>#</th>
-                                          <th>Name</th>
-                                          <th>Continent</th>
-                                          <th>Region</th>
-                                          <th>SurfaceArea</th>
-                                          <th>link</th>
-                                      </tr>
-                                  </thead>
-                                  <tbody>
-                                      <?php foreach($allRecords as $k=>$record){?>
-                                      <tr>
-                                          <td><?php echo $k +1 ?></td>
-                                          <td><?php echo $record["Name"] ?></td>
-                                          <td><?php echo $record["Continent"] ?></td>
-                                          <td><?php echo $record["Region"] ?></td>
-                                          <td><?php echo $record["SurfaceArea"] ?></td>
-                                          <td><a href="cities.php?q=<?php echo $record["Code"]?>">
-                                                   View Cities
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-12">
+                <div class="card shadow border">
+                    <div class="card-body">
+                        <h2 class="text-center">Countries</h2>
+                        <hr>
+                        <?php include_once("success.php") ?>
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Continent</th>
+                                    <th>Region</th>
+                                    <th>SurfaceArea</th>
+                                    <th>link</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($allRecords as $k => $record) { ?>
+                                    <tr>
+                                        <td><?php echo $k + 1 ?></td>
+                                        <td><?php echo $record["Name"] ?></td>
+                                        <td><?php echo $record["Continent"] ?></td>
+                                        <td><?php echo $record["Region"] ?></td>
+                                        <td><?php echo $record["SurfaceArea"] ?></td>
+                                        <td><a href="cities.php?q=<?php echo $record["Code"] ?>">
+                                                View Cities
                                             </a></td>
-                                      </tr>
-                                      <?php }?>
-                                  </tbody>
-                            </table>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                        <div class="text-center mt-1">
+                            <button id="loadMoreBtn" class="btn btn-success">
+                                Load More
+                                <i id="loadMoreSpinner" class="fas fa-spinner fa-spin d-none"></i>
+                            </button>
                         </div>
-                   </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>    
 
 
 
-<script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/all.min.js"></script>
+    <script src="../js/jquery.js"></script>
+    <script>
+        function generateCountryRow(n,record){
+            var html='';
+            return html;
+        }
+        $(document).ready(function() {
+            var counter = 20;
+            $("#loadMoreBtn").click(function(ev) {
+                ev.preventDefault();
+                var loadMoreSpinner = $("#loadMoreSpinner");
+                loadMoreSpinner.removeClass("d-none");
+                $(this).attr("disabled", "disabled");
+
+                $.ajax({
+                    url: "/php_lec1/database/countries.ajax.php?c=" + counter,
+                    method: "GET",
+                    success: function(result) {
+                        var countries = JSON.parse(result);
+                        if (countries) {
+
+                        }
+
+
+                        counter += 20;
+
+                    },
+                    error: function() {
+
+                    },
+                    complete: function() {
+                        $("#loadMoreSpinner").addClass("d-none");
+                        $("#loadMoreBtn").removeAttr("disabled");
+                    }
+
+                })
+            })
+        })
+    </script>
 </body>
+
 </html>
